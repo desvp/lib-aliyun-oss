e18579aecd0b00035a6c6a0a6279a751cf72ec0b
Combo <combo@desvp.com>
2015-06-18 02:02:40 +0800

    add: __init__

---
+++ liboss/__init__.py
@@ -0,0 +1,24 @@
+# vim: set fileencoding=utf-8 :
+
+
+__title__ = 'liboss'
+
+__version__ = '0.1.0'
+VERSION = tuple(map(int, __version__.split('.')))
+
+__author__ = 'Combo'
+
+__license__ = 'MIT License'
+
+__short_description__ = '''
+liboss is a free and open-source utility
+that provides Aliyun OSS management.
+'''.replace('\n', ' ')
+
+__long_description__ = '''
+liboss can help you to manage buckets (list, create, delete),
+and manage files (upload, download, copy, delete) with Aliyun OSS.
+'''.replace('\n', ' ')
+
+
+# vim:nocp:tw=120:sw=4:ts=4:
